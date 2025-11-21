707ccdb6f4bb03d1594d51a5948d9c71d0819cb8
Dario Ureña <darioaxel@gmail.com>
2025-11-21 13:03:08 +0100

    problemas con los css

---
+++ app/components/landing/MyHero.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+const title = 'The Foundation for your Design System'
+const description
+  = 'A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,  
+})
+</script>
+
+
+<template>
+  <div class="flex flex-1 flex-col">
+   <PageHeaderHeading class="max-w-4xl">
+        {{ title }}
+      </PageHeaderHeading>
+      <PageHeaderDescription>{{ description }}</PageHeaderDescription>
+      <PageActions>
+        <Button as-child size="sm">
+          <NuxtLink to="/">
+            Get Started
+          </NuxtLink>
+        </Button>
+        <Button as-child size="sm" variant="ghost">
+          <NuxtLink to="/">
+            View Components
+          </NuxtLink>
+        </Button>
+      </PageActions>
+      </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="container">
-        <h1>Welcome to Nuxt 4 Boilerplate</h1>
-        <p>This is the main page of your Nuxt 4 application.</p>
-        <Button>Click me</Button>
+        <LandingMyHero />
     </div>
 </template>
